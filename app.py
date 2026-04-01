e34bbccaab0cabbc683e56d03f632ceae28c5270
ASAD AZIZ <asadaziz40p@gmail.com>
2026-04-01 18:36:39 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 from transformers import pipeline
+import warnings
+warnings.filterwarnings('ignore')
 
 # ---- PAGE CONFIG ----
 st.set_page_config(page_title="AI Text Summarizer", layout="centered")
